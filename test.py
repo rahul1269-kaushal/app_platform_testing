cb446f2a50d37ca8ce145841852276f26b14f200
rahul1269-kaushal <59966780+rahul1269-kaushal@users.noreply.github.com>
2024-01-09 23:37:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    return render_template("/index.html")
